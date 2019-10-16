7a273f56cd5cf9b2322c4e711dd566245301952d
jaden.liao <jaden.liao@zoom.us>
2019-10-16 19:36:09 +0800

    create project

---
+++ pages/index/index.vue
@@ -0,0 +1,66 @@
+<template>
+	<view class="content">
+		<image class="logo" src="/static/logo.png"></image>
+		<view class="text-area">
+			<text class="title">{{title}}</text>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello'
+			}
+		},
+		onLoad() {
+			this.getData();
+		},
+		methods: {
+			getData() {
+				uni.request({
+				    url: '/api/find_column_link', //仅为示例，并非真实接口地址。
+				    data: {
+				        // text: 'uni.request'
+				    },
+				    header: {
+				        // 'custom-header': 'hello' //自定义请求头信息
+				    },
+				    success: (res) => {
+				        console.log(res.data);
+				        this.text = 'request success';
+				    }
+				});
+			}
+		}
+	}
+</script>
+
+<style>
+	.content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+
+	.logo {
+		height: 200rpx;
+		width: 200rpx;
+		margin-top: 200rpx;
+		margin-left: auto;
+		margin-right: auto;
+		margin-bottom: 50rpx;
+	}
+
+	.text-area {
+		display: flex;
+		justify-content: center;
+	}
+
+	.title {
+		font-size: 36rpx;
+		color: #8f8f94;
+	}
+</style>
